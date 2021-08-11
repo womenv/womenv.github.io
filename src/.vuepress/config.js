818e32119cde0d611fccf742b253f009a52373ff
w <women.voice@outlook.com>
2021-08-11 20:45:57 +0800

    init

---
+++ src/.vuepress/config.js
@@ -0,0 +1,83 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: '女性声音',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: '声音',
+        link: '/voice/',
+      },
+      {
+        text: '新闻事实',
+        link: '/thefact/'
+      },
+      {
+        text: '书单',
+        link: '/booklist/'
+      },
+      {
+        text: '关于',
+        link: '/about/'
+      },
+    ],
+    sidebar: {
+      '/voice/': [
+        {
+          title: '声音',
+          collapsable: false,
+          children: [
+            '0001.md',
+            '0002.md'
+          ]
+        },
+      ],
+      '/thefact/': [
+        {
+          title: '新闻事实',
+          collapsable: false,
+          children: [
+            '0001.md',
+          ]
+        },
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
---
+++ src/index.md
@@ -0,0 +1,39 @@
+---
+home: false
+heroImage: https://v1.vuepress.vuejs.org/hero.png
+tagline: 
+actionText: Quick Start →
+actionLink: /guide/
+features:
+- title: Feature 1 Title
+  details: Feature 1 Description
+- title: Feature 2 Title
+  details: Feature 2 Description
+- title: Feature 3 Title
+  details: Feature 3 Description
+footer: Made by  with ❤️
+---
+
+## 卷首
+
+有些女性爱说女人对女人恶意最大，而说这句话的人，你是否忘记了自己也是女性，你扪心自问一下，你对女性的恶意大吗？
+
+如果大，当我没说。
+
+如果不大，那我想知道，为什么你在说这句话的时候把自己排除在女性之外了呢？
+
+生而为女数十载，却意识不到自己也是女性，这就是明明生理为女的女性那么多，心理为女的女性却这么少吧。
+
+之前也在被删的帖子里分享过男人是如何看待女人的，可以是工具是俘虏是奴隶是狗，但绝不是人。
+
+所以还是希望同为女性的群体，能把不喜欢的女性当做一个男人来看待，哦不，是人来看待。
+
+在说A对我恶意大的时候，就说A，别把“女”字当做贬义词，说成“女A”，这样和那些嘲讽女司机的媒体有什么两样？
+
+“遇到不好的男领导：fuck！果然领导就是令人讨厌；遇到不好的女领导：fuck！果然女的就是令人讨厌！”
+
+俄罗斯哪怕无法融入东方，西方还是觉得俄罗斯是东方。你哪怕不认同自己是女性，男性也仍然觉得你是女性，没有例外。
+
+[https://www.douban.com/group/topic/241902115/](https://www.douban.com/group/topic/241902115/)
+
+[继续阅读 ->](/voice)
