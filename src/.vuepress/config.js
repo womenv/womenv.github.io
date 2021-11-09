607c1b578c8cad9c18221fe37f22e8cd7f0cb794
womenv <88717419+womenv@users.noreply.github.com>
2021-11-09 19:53:42 +0800

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -67,7 +67,7 @@ module.exports = {
         {
           title: '声音',
           collapsable: false,
-          children: genSidebarChildren(19)
+          children: genSidebarChildren(22)
         },
       ],
       '/thefact/': [
